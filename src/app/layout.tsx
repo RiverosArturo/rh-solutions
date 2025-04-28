7cdb873a4aada882ccc4679330bdf481da7a2e91
Arturo Riveros <riveros45rturo@gmail.com>
2025-04-28 02:36:23 -0600

    Google search console

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,9 @@ export const metadata: Metadata = {
   metadataBase: new URL("https://rhsolutionsmx.com/"),
   title: "RH Solutions",
   description: "Soluciones digitales en desarrollo web y apps",
+  verification: {
+    google: "nou6mVBxa46Ace5aSDUfQyyxcyBtn8RKncyZ3GDvUTc",
+  },
 };
 
 export default function RootLayout({ children }: { children: ReactNode }) {
